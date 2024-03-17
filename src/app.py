384d5e8397da74c689db9dbb01a2bbd1ac67677b
anmatngu <75156243+anmatngu@users.noreply.github.com>
2024-03-18 01:25:49 +0700

    Done

--- src/app.py
+++ src/app.py
@@ -1,41 +1,51 @@
 import streamlit as st
-from sklearn.feature_extraction.text import TfidfVectorizer
 import joblib
+import difflib
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
-loaded_model = joblib.load('src\\spam_detection_model.pkl')
+movies_df = pd.read_csv('data\\movies.csv')
+
 feature_extraction = joblib.load('src\\feature_extraction.pkl')
+list_of_all_titles = movies_df['title'].tolist()
+similarity = cosine_similarity(feature_extraction)
 
-def mail_detection(user_input):
+def movie_suggestion(user_input):
     # Vectorize the email text
-        input_mail = str(user_input)
+        input_movie = str(user_input)
+        find_close_match = difflib.get_close_matches(input_movie, list_of_all_titles)
+        print(find_close_match)
+        close_match = find_close_match[0]
+        print(close_match)
+        index_of_the_movie = movies_df[movies_df['title'] == close_match]['index'].values[0]
+        print(index_of_the_movie)
+        similarity_score = list(enumerate(similarity[index_of_the_movie]))
+        sorted_similar_movies = sorted(similarity_score, key = lambda x:x[1], reverse = True)
+        print('Movies suggested for you : \n')
 
-        # convert text to feature vectors
-        input_data_features = feature_extraction.transform([input_mail])
-
-        # making prediction
-
-        prediction = loaded_model.predict(input_data_features)
-        spam_proba = loaded_model.predict_proba(input_data_features)[0][1]
-        print(prediction)
-
-
-        if prediction[0] == 1:
-            st.write("This email is classified as SPAM.")
-            st.write(f"Spam Probability: {(spam_proba):.2f}")
-        else:
-            st.write("This email is classified as Not SPAM.")
-            st.write(f"Spam Probability: {(1 - spam_proba):.2f}")
-
+        i = 1
+        max_suggestion = 10
+        movie_list = 'You should watch '
+        for movie in sorted_similar_movies:
+            index = movie[0]
+            title_from_index = movies_df[movies_df['index']==index]['title'].values[0]
+            release_date_index = movies_df[movies_df['index']==index]['release_date'].values[0]
+            if (i <= max_suggestion):
+                movie_list += '\n' + str(i) + '. ' + title_from_index + ' - ' + f'({release_date_index})'
+                i+=1
+        return movie_list
 def main():
     """
     Main function for the Streamlit app.
     """
 
-    st.title("Spam Detection App")
-    email_text = st.text_area("Enter Email Text:", height=200)
+    st.title("Movie Suggestion")
+    movie_text = st.text_area("Enter movie name:", height=50)
 
     if st.button("Classify"):
-        mail_detection(email_text)
+        suggestion = movie_suggestion(movie_text)
+    
+    st.write(suggestion)
 
 if __name__ == '__main__':
     main()
